3f7c15cd5ee32ee414949850b7b64e7ab91d1b82
atiholoz <andre.tikholoz@s-telematics.ru>
2020-08-06 19:21:41 +0300

    add some test replyes;

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 
 const { Telegraf } = require('telegraf')
+// eslint-disable-next-line no-unused-vars
+const Telegram = require('./telegram.js')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.on('sticker', async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.chat, null, '\t'))
+  await ctx.reply(JSON.stringify(ctx.update, null, '\t'))
+  await ctx.reply(JSON.stringify(ctx.message, null, '\t'))
+})
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 bot.launch()
